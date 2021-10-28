48cd7f658406f675b8e9b878a059f3b4b83c67fa
Ricoding <rikardoan@gmail.com>
2021-10-27 22:05:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,26 +23,26 @@ function init() {
       {
         type: 'input',
         name: 'projectN',
-        message: 'What is the name of the Project?',
+        message: 'Provide the name of the project.',
       },
       {
         type: 'input',
         name: 'descripInput',
-        message: 'Provide a short description?',
+        message: 'Provide a short description.',
       },   
        {
         type: 'input',
         name: 'installat',
-        message: 'Provide a installation?',
+        message: 'Provide the installation requirements.',
       },
       {
         type: 'input',
         name: 'usage',
-        message: 'Provide instructions and examples for use',
+        message: 'Provide instructions and examples for use.',
       },
       {
         type: 'checkbox',
-        message: 'Select the license?',
+        message: 'Select the license you want to use.',
         name: 'lic',
         choices: ['Apache', 'GNU', 'MIT', 'none'],
       },
